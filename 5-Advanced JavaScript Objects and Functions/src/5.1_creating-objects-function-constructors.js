aace0c54ecdf966b279ad95440cfb75815f3cdd5
mrtdmr <muratdemir.pau@gmail.com>
2019-12-19 16:58:49 +0300

    191219_1658

--- 5-Advanced JavaScript Objects and Functions/src/5.1_creating-objects-function-constructors.js
+++ 5-Advanced JavaScript Objects and Functions/src/5.1_creating-objects-function-constructors.js
@@ -4,3 +4,21 @@ var john = {
   yearOfBirth: 1990,
   job: "teacher"
 };
+var Person = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+Person.prototype.calculateAge = function() {
+  console.log(2016 - this.yearOfBirth);
+};
+Person.prototype.lastName = "Smith";
+var john = new Person("john", 1990, "teacher"); //Instantiation. new operatoru yeni bir nesne oluşturur ve fonksiyonun yapıcı metoduna bu yeni nesneyi aktarır. Bu sayede this anahtar kelimesi global context i değil gönderilen nesneyi işaret eder.
+var jane = new Person("Jane", 1969, "designer");
+var mark = new Person("Mark", 1948, "retired");
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
